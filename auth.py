db734255628b146132c0f33de12275179e27e886
Avni Mishra <avni.mishra0409@gmail.com>
2026-04-10 17:52:23 -0400

    gmail client

--- auth.py
+++ auth.py
@@ -15,7 +15,7 @@ Photos:
 '''
 
 SCOPES =    [
-            'https://www.googleapis.com/auth/gmail.readonly',
+            'https://www.googleapis.com/auth/gmail.modify',
             'https://www.googleapis.com/auth/photoslibrary.appendonly',
             'https://www.googleapis.com/auth/photoslibrary.readonly.appcreateddata',
             ]
